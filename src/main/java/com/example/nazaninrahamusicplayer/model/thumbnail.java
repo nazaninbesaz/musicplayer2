2b8b60f1f81cdb1630415204ce59a2e232acb56f
nazaninbesaz <122652833+nazaninbesaz@users.noreply.github.com>
2023-01-14 04:50:10 +0330

    third commit

---
+++ src/main/java/com/example/nazaninrahamusicplayer/model/thumbnail.java
@@ -0,0 +1,14 @@
+package com.example.arghavanazizimusicplayer.model;
+
+public class thumbnail
+{
+    String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
